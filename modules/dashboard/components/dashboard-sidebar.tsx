655e7ec89923f49f4ab72ccb4791da440e3b5434
Malikusman333 <wwwusmancom745@gmail.com>
2026-09-15 15:02:39 +0500

    07-Playgorund-Functionality

--- modules/dashboard/components/dashboard-sidebar.tsx
+++ modules/dashboard/components/dashboard-sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import {
@@ -69,6 +69,11 @@ export function DashboardSidebar({
   const [recentPlaygrounds, setRecentPlaygrounds] = useState(
     initialPlaygroundData,
   );
+
+useEffect(() => {
+  setRecentPlaygrounds(initialPlaygroundData);
+  setStarredPlaygrounds(initialPlaygroundData.filter((p) => p.starred));
+}, [initialPlaygroundData]);
 
   return (
     <Sidebar
